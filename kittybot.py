1c7e9bac436bbf637f825ead77b79331b3cf22eb
Shamil Salakhov <debilsalakhov@yandex.ru>
2024-10-26 21:46:34 +0300

    v1

---
+++ kittybot.py
@@ -0,0 +1,66 @@
+import logging
+import requests
+import os
+
+from telebot import TeleBot, types
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+secret_token = os.getenv('TOKEN')
+bot = TeleBot(token=secret_token)
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+)
+
+URL = 'https://api.thecatapi.com/v1/images/search'
+
+
+def get_new_image():
+    try:
+        response = requests.get(URL)
+    except Exception as error:
+        logging.error(f'Ошибка при запросе к основному API: {error}')
+        new_url = 'https://api.thedogapi.com/v1/images/search'
+        response = requests.get(new_url)
+    response = response.json()
+    random_cat = response[0].get('url')
+    return random_cat
+
+
+@bot.message_handler(commands=['newcat'])
+def new_cat(message):
+    chat = message.chat
+    bot.send_photo(chat.id, get_new_image())
+
+
+@bot.message_handler(commands=['start'])
+def wake_up(message):
+    chat = message.chat
+    name = message.chat.first_name
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button_newcat = types.KeyboardButton('/newcat')
+    keyboard.add(button_newcat)
+    bot.send_message(chat_id=chat.id,
+                     text=f'Hello, {name}',
+                     reply_markup=keyboard)
+    bot.send_photo(chat.id, get_new_image())
+
+
+@bot.message_handler()
+def say_hi(message):
+    chat = message.chat
+    chat_id = chat.id
+    bot.send_message(chat_id=chat_id, text='XYU')
+
+
+def main():
+    bot.polling()
+
+
+if __name__ == '__main__':
+    main()
